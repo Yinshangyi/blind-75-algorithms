f55ec47410cd925d980a62a89861ff0074364f45
Yinshangyi <jl.dejaeger@gmail.com>
2022-09-05 20:23:33 +0200

    product_except_self

--- test/arrays/test_product_except_self.py
+++ test/arrays/test_product_except_self.py
@@ -11,3 +11,15 @@ def testProductExceptSelf2():
     array = [-1, 1, 0, -3, 3]
     productResults = Solution().productExceptSelf(array)
     assert productResults == [0, 0, 9, 0, 0]
+
+
+def testProductExceptSelfOptimized1():
+    array = [1, 2, 3, 4]
+    productResults = Solution().productExceptSelfOptimized(array)
+    assert productResults == [24, 12, 8, 6]
+
+
+def testProductExceptSelfOptimized2():
+    array = [-1, 1, 0, -3, 3]
+    productResults = Solution().productExceptSelfOptimized(array)
+    assert productResults == [0, 0, 9, 0, 0]
