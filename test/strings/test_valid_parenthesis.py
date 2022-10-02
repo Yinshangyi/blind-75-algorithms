5ab2e4ea3b1516f99bd30b6935bd9681cfb828ee
Yinshangyi <jl.dejaeger@gmail.com>
2022-10-02 18:15:54 +0200

    Valid Parenthesis

---
+++ test/strings/test_valid_parenthesis.py
@@ -0,0 +1,19 @@
+from src.strings.valid_parenthesis import Solution
+
+
+def testValidParenthesis1():
+    inputString = "()"
+    areParenthesisValid = Solution().isValid(inputString)
+    assert areParenthesisValid == True
+
+
+def testValidParenthesis2():
+    inputString = "()[]{}"
+    areParenthesisValid = Solution().isValid(inputString)
+    assert areParenthesisValid == True
+
+
+def testValidParenthesis3():
+    inputString = "(]"
+    areParenthesisValid = Solution().isValid(inputString)
+    assert areParenthesisValid == False
