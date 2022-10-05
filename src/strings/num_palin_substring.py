7ad54ddfb64c32b8cf214fa9b88c63382b0e20ba
Yinshangyi <jl.dejaeger@gmail.com>
2022-10-05 14:09:11 +0200

    Count Palindromic Substrings

--- src/strings/num_palin_substring.py
+++ src/strings/num_palin_substring.py
@@ -1,4 +1,16 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:
+    def countPalindrome(self, string, leftPointer, rightPointer):
+        numPalindrome = 0
+        while leftPointer >= 0 and rightPointer < len(string) \
+                and string[leftPointer] == string[rightPointer]:
+            numPalindrome += 1
+            leftPointer -= 1
+            rightPointer += 1
+        return numPalindrome
 
-        return 0
+    def countSubstrings(self, string: str) -> int:
+        numPalindrome = 0
+        for index in range(0, len(string)):
+            numPalindrome += self.countPalindrome(string, index, index)
+            numPalindrome += self.countPalindrome(string, index, index + 1)
+        return numPalindrome
