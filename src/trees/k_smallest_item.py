6fa5d2cc58387ee119536b684ffe17a3c609a237
Yinshangyi <jl.dejaeger@gmail.com>
2022-10-15 17:13:36 +0200

    K-smallest element

--- src/trees/k_smallest_item.py
+++ src/trees/k_smallest_item.py
@@ -5,21 +5,18 @@ from src.trees.utils import TreeNode
 
 
 class Solution:
-    def addToStackLeftElements(self, root: Optional[TreeNode], stack: deque) -> deque:
+    def addToStackInOrderElements(self, root: Optional[TreeNode], stack: deque) -> deque:
         if root:
-            self.addToStackLeftElements(root.left, stack)
+            self.addToStackInOrderElements(root.left, stack)
             stack.append(root)
+            self.addToStackInOrderElements(root.right, stack)
         return stack
 
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         stack = deque()
-        stackResult = self.addToStackLeftElements(root, stack)
+        stackResult = self.addToStackInOrderElements(root, stack)
         kSmallestElement: TreeNode = TreeNode(None)
         while len(stackResult) > 0 and k > 0:
             kSmallestElement = stackResult.popleft()
             k -= 1
-            if k > 0:
-                kSmallestElement = kSmallestElement.right
-                if kSmallestElement:
-                    k -= 1
         return kSmallestElement.val
