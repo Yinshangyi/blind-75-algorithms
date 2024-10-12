b25637e610055e3aa36e450c9044464d7e845d9b
Yinshangyi <jl.dejaeger@gmail.com>
2024-10-12 23:59:13 +0200

    delete unnecessary files

--- python/test/strings/test_encode.py
+++
@@ -1,7 +0,0 @@
-from src.strings.encode import Solution
-
-
-def testEncode1():
-    inputString = "Hello Jennny"
-    compressedString = Solution().compressString(inputString)
-    assert compressedString == "Hel2o Jen3y"
