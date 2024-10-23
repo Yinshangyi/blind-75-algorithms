9020973548c433e486bd4cbb4d2c6c0d1d4792aa
Yinshangyi <jl.dejaeger@gmail.com>
2024-10-23 15:52:33 +0200

    Container with most water

--- python/test/arrays/container_with_most_water.py
+++ python/test/arrays/container_with_most_water.py
@@ -1,8 +1,24 @@
-from src.arrays.container_with_most_water import Solution
+from typing import List
+
+import pytest
+
+from src.arrays.container_most_water.most_water_finder import MostWaterFinder
+from src.arrays.container_most_water.most_water_finder_fp import MostWaterFinderFP
+from src.arrays.container_most_water.most_water_finder_imp import MostWaterFinderImp
 
 
-def test_container_with_most_water():
-    heights = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-    best_amount = Solution().maxArea(heights)
-    expected_best_amount = 49
-    assert best_amount == expected_best_amount
+@pytest.fixture(params=[
+    MostWaterFinderImp,
+    MostWaterFinderFP
+])
+def area_finder(request: pytest.FixtureRequest):
+    return request.param()
+
+
+@pytest.mark.parametrize("heights, expected_area", [
+    ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+    ([1, 1], 1)
+])
+def test_should_return_the_maximum_water_area(area_finder: MostWaterFinder, heights: List[int], expected_area: int):
+    best_amount = area_finder.max_area(heights)
+    assert best_amount == expected_area
