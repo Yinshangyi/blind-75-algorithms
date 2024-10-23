746dff097ed4d7d50936165aed79ec5faa9b58bf
Yinshangyi <jl.dejaeger@gmail.com>
2024-10-23 12:59:16 +0200

    Three sums

--- python/test/arrays/test_three_sum.py
+++ python/test/arrays/test_three_sum.py
@@ -1,10 +1,24 @@
-from src.arrays.three_sum import Solution
+from typing import List
+
+import pytest
+
+from src.arrays.three_sums.three_sums_finder import ThreeSumsFinder
+from src.arrays.three_sums.three_sums_finder_imp import ThreeSumsFinderImp
 
 
-def testThreeSums():
-    nums = [-1, 0, 1, 2, -1, -4]
-    expectedResult = [[-1, -1, 2], [-1, 0, 1]]
-    result = Solution().threeSum(nums)
-    assert result == expectedResult
+@pytest.fixture(params=[
+    ThreeSumsFinderImp
+])
+def three_sums_finder(request: pytest.FixtureRequest):
+    return request.param()
 
 
+@pytest.mark.parametrize("nums, target, exp_result", [
+    ([-1, 0, 1, 2, -1, -4], 0, [[-1, -1, 2], [-1, 0, 1]])
+])
+def test_should_return_the_indexes_of_three_items_making_the_sum_equal_to_target(three_sums_finder: ThreeSumsFinder,
+                                                                                 nums: List[int],
+                                                                                 target: int,
+                                                                                 exp_result: List[List[int]]):
+    result = three_sums_finder.three_sum(nums, target)
+    assert result == exp_result
