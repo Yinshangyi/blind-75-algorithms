39a93116d6c09ec441803b1beaa493d27b408aec
Yinshangyi <jl.dejaeger@gmail.com>
2024-10-24 12:10:28 +0200

    Test Remove n items from Linked List

--- python/test/linkedlists/test_remove_n_items.py
+++ python/test/linkedlists/test_remove_n_items.py
@@ -1,35 +1,30 @@
-from src.linkedlists.remove_n_items import Solution
+from typing import List
+
+import pytest
+
+from src.linkedlists.remove_n_items.n_items_remover import NItemsRemover
+from src.linkedlists.remove_n_items.n_items_remover_imp import NItemsRemoverImp
 from src.linkedlists.utils import ListNode
 from test.linkedlists.utils import array_2_linked_list, linked_list_2_array
 
 
-def testNLastElementsFromLinkedList1():
-    inputList: ListNode = array_2_linked_list([1, 2, 3, 4, 5])
-    k = 2
-    listWithRemovedElements: ListNode = Solution().removeNthFromEnd(inputList, k)
-    expectedList = [1, 2, 3, 5]
-    assert linked_list_2_array(listWithRemovedElements) == expectedList
+@pytest.fixture(params=[
+    NItemsRemoverImp
+])
+def item_remover(request: pytest.FixtureRequest):
+    return request.param()
 
 
-def testNLastElementsFromLinkedList2():
-    inputList: ListNode = array_2_linked_list([1])
-    k = 1
-    listWithRemovedElements: ListNode = Solution().removeNthFromEnd(inputList, k)
-    expectedList = []
-    assert linked_list_2_array(listWithRemovedElements) == expectedList
-
-
-def testNLastElementsFromLinkedList3():
-    inputList: ListNode = array_2_linked_list([1, 2])
-    k = 1
-    listWithRemovedElements: ListNode = Solution().removeNthFromEnd(inputList, k)
-    expectedList = [1]
-    assert linked_list_2_array(listWithRemovedElements) == expectedList
-
-
-def testNLastElementsFromLinkedList4():
-    inputList: ListNode = array_2_linked_list([1, 2])
-    k = 2
-    listWithRemovedElements: ListNode = Solution().removeNthFromEnd(inputList, k)
-    expectedList = [2]
-    assert linked_list_2_array(listWithRemovedElements) == expectedList
+@pytest.mark.parametrize("input_list, k, exp_list", [
+    ([1, 2, 3, 4, 5], 2, [1, 2, 3, 5]),
+    ([1], 1, []),
+    ([1, 2], 1, [1]),
+    ([1, 2], 2, [2])
+])
+def test_should_return_a_list_with_n_item_removed_from_the_input_list(item_remover: NItemsRemover,
+                                                                      input_list: List[int],
+                                                                      k: int,
+                                                                      exp_list: List[int]):
+    input_list: ListNode = array_2_linked_list(input_list)
+    listWithRemovedElements: ListNode = item_remover.remove_nth_from_end(input_list, k)
+    assert linked_list_2_array(listWithRemovedElements) == exp_list
